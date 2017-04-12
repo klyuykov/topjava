c88cb5533bae95299ffb508580276ce591c22cb9
Michael <klyuykov@gmail.com>
2017-04-12 21:28:34 +0300

    HW2

---
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -0,0 +1,51 @@
+package ru.javawebinar.topjava.repository.mock;
+
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@Repository
+public class InMemoryUserRepositoryImpl implements UserRepository {
+    private Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    @Override
+    public boolean delete(int id) {
+        return true;
+    }
+
+    @Override
+    public User save(User user) {
+        if(user.isNew()) {
+            user.setId(counter.incrementAndGet());
+        }
+        repository.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User get(int id) {
+        return repository.get(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        repository.entrySet().stream().map(m -> m.getValue()).collect(Collectors.toList());
+        repository.entrySet().stream()
+                .map(m -> m.getValue())
+                .collect(Collectors.toList());
+        return Collections.emptyList();
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        return null;
+    }
+}
